b9544b72a0a98925d92c68f85f57687de009bef1
Eduardo Martin-Sonseca <martinsonsecaeduardo@gmail.com>
2023-12-04 11:33:37 +0100

    NTT DATA

---
+++ 2do_DAM/DI/2da Evaluación/Ejemplos/CreacionAplicacion/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CreacionAplicacion
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
