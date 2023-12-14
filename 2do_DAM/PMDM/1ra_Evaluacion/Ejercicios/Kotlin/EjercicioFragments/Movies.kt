c63b61ee80497c71de226907e693c091a0013e17
Eduardo Martin-Sonseca <martinsonsecaeduardo@gmail.com>
2023-12-14 13:53:07 +0100

    rt

---
+++ 2do_DAM/PMDM/1ra_Evaluacion/Ejercicios/Kotlin/EjercicioFragments/Movies.kt
@@ -0,0 +1,13 @@
+package com.example.ejerciciopeliculas
+
+class Movies {
+    companion object {
+        var datos:ArrayList<Movie> = arrayListOf<Movie>(
+            Movie("El Padrino", "El Padrino es una película estadounidense de 1972 dirigida por Francis Ford Coppola. El filme fue producido por Albert S. Ruddy, de la compañía Paramount Pictures. El guion fue adaptado por Coppola y Mario Puzo a partir de la novela homónima de Puzo.", 1972),
+            Movie("El Padrino II", "El Padrino II es una película estadounidense de 1974 dirigida por Francis Ford Coppola. El filme fue producido por Albert S. Ruddy, de la compañía Paramount Pictures. El guion fue adaptado por Coppola y Mario Puzo a partir de la novela homónima de Puzo.", 1974),
+            Movie("El Padrino III", "El Padrino III es una película estadounidense de 1990 dirigida por Francis Ford Coppola. El filme fue producido por Albert S. Ruddy, de la compañía Paramount Pictures. El guion fue adaptado por Coppola y Mario Puzo a partir de la novela homónima de Puzo.", 1990),
+            Movie("El Padrino IV", "El Padrino IV es una película estadounidense de 1992 dirigida por Francis Ford Coppola. El filme fue producido por Albert S. Ruddy, de la compañía Paramount Pictures. El guion fue adaptado por Coppola y Mario Puzo a partir de la novela homónima de Puzo.", 1992),
+            Movie("El Padrino V", "El Padrino V es una película estadounidense de 1994 dirigida por Francis Ford Coppola. El filme fue producido por Albert S. Ruddy, de la compañía Paramount Pictures. El guion fue adaptado por Coppola y Mario Puzo a partir de la novela homónima de Puzo.", 1994),
+            Movie("El Padrino VI", "El Padrino VI es una película estadounidense de 1996 dirigida por Francis Ford Coppola. El filme fue producido por Albert S. Ruddy, de la compañía Paramount Pictures. El guion fue adaptado por Coppola y Mario Puzo a partir de la novela homónima de Puzo.", 1996))
+    }
+}
