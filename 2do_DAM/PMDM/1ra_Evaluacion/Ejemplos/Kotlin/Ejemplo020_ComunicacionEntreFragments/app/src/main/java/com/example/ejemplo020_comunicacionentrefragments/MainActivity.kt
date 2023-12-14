565c8121178b978109321ce3fe4b9876041fab87
Eduardo Martin-Sonseca <martinsonsecaeduardo@gmail.com>
2023-12-14 12:28:57 +0100

    A

--- 2do_DAM/PMDM/1ra_Evaluacion/Ejemplos/Kotlin/Ejemplo020_ComunicacionEntreFragments/app/src/main/java/com/example/ejemplo020_comunicacionentrefragments/MainActivity.kt
+++ 2do_DAM/PMDM/1ra_Evaluacion/Ejemplos/Kotlin/Ejemplo020_ComunicacionEntreFragments/app/src/main/java/com/example/ejemplo020_comunicacionentrefragments/MainActivity.kt
@@ -2,10 +2,11 @@ package com.example.ejemplo020_comunicacionentrefragments
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.ejemplo020_comunicacionentrefragments.databinding.FragmentSuperiorBinding
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var fragmentSuperior: FragmentSuperior
+    lateinit var fragmentSuperior: FragmentSuperiorBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
