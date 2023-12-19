96388b01c3777272980b3ae31b51e82166b147c8
Eduardo Martin-Sonseca <martinsonsecaeduardo@gmail.com>
2023-12-19 11:07:02 +0100

    DI

---
+++ 2do_DAM/DI/2da Evaluacion/01-NET MAUI/Ejercicios/AppMAUI01/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AppMAUI01
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ 2do_DAM/DI/2da Evaluacion/01-NET MAUI/Ejercicios/AppMAUI01/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace AppMAUI01
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
