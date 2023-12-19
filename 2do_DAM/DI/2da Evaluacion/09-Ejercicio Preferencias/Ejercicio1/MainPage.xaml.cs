1e6d4daf328affee0c931c391f6308553fa04b77
Eduardo Martin-Sonseca <martinsonsecaeduardo@gmail.com>
2023-12-19 13:52:09 +0100

    DI

---
+++ 2do_DAM/DI/2da Evaluacion/09-Ejercicio Preferencias/Ejercicio1/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿
+namespace Ejercicio1
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            if (editNombre.MaxLength > 10 || editNombre.MaxLength < 10)
+            {
+                TextoErroneo.Text = "(*) Esta casilla es obligatoria";
+
+            }
+        }
+    }
+
+}
